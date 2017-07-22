af11e34f6e3bcdfc1103b92d460a57b4746824a0
Henry Steinitz <Hjsteinitz@gmail.com>
2017-07-22 16:56:38 -0400

    Better modularity and more readme edits

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Alex Graves, Greg Wayne, and Ivo Danihelka at DeepMind in 2014. It's essentially
 a Turing Machine whose finite-state controller is replaced
 with a simple feedforward or recurrent neural network. Differentiable versions
 of the read/write operations are then attached to the new controller, allowing
-the system to compute error derivatives using backpropagation. Thus, the entire 
-Neural Turing Machine can learn by gradient descent.
+the system to compute error derivatives using backpropagation. This means the
+entire Neural Turing Machine can learn by gradient descent.
 
 ## How It Works
--- modules.py
+++ modules.py
@@ -1,17 +1,34 @@
 from math import cosine_sim
 from functools import partial
 
-def control(inp, read_out):
-    # Emits the following:
-    # read_key_vector, write_key_vector,
-    # read_key_strength, write_key_strength,
-    # read_interpolation_gate, write_interpolation_gate,
-    # read_shift_weighting, write_shift_weighting,
-    # read_sharpener, write_sharpener,
-    # output
+# ---Control Modules---
+
+def lstm_layer(inp, weights):
+    return
+
+def feedforward_layer(inp, weights):
+    return tf.nn.sigmoid(tf.matmul(inp, weights))
+
+def key_vector_out(hidden_state, weights):
+    return feedforward_layer(hidden_state, weights)
+
+def key_strengh_out(hidden_state, weights):
+    return feedforward_layer(hidden_state, weights)
+
+def shift_weighting_out(hidden_state, weights):
+    return tf.nn.softmax(tf.matmul(hidden_state, weights))
+
+def sharpener_out(hidden_state, weights):
+    return 1 / feedforward_layer(hidden_state, weights)
+
+def control(inp, read_out, hidden_weights, kind='feedforward', io_weights):
     controller_input = tf.concat(inp, read_out)
-    hidden_activation = tf.nn.sigmoid(tf.matmul(controller_input, controller_weights_1))
-    controller_output = tf.nn.sigmoid(tf.matmul(hidden_activation, controller_weights_2))
+    hidden_state = feedforward_layer(controller_input, hidden_weights)
+
+    key_vector = key_vector_out(hidden_state, io_weights['key_vector'])
+    key_strength = key_strengh_out(hidden_state, io_weights['key_strength'])
+
+    re
     return = tf.split(controller_output, [
     # read_key_vector, write_key_vector,
     # read_key_strength, write_key_strength,
@@ -21,32 +38,27 @@ def control(inp, read_out):
     # output
     ])
 
+
+# ---Focus Modules---
+
 def content_focus(memory_matrix, key_vector, key_strengh):
     sim_vector = key_strengh * tf.map_fn(partial(cosine_sim, key_vector), memory_matrix)
     return tf.nn.softmax(sim_vector)
 
-def interpolate(read_interpolation_gate, w_c_r, prev_w_r,
-                write_interpolation_gate, w_c_w, prev_w_w):
-    w_g_r = read_interpolation_gate * w_c_r +
-            (1 - read_interpolation_gate) * prev_w_r
-    w_g_w = write_interpolation_gate * w_c_w +
-            (1 - write_interpolation_gate) * prev_w_w
-    return w_g_r, w_g_w
+def interpolate(read_interpolation_gate, w_c, prev_w):
+    return interpolation_gate * w_c + (1 - interpolation_gate) * prev_w
 
-def shift(w_g_r, read_shift_weighting, w_g_w, write_shift_weighting):
-
-def sharpen(w_shift_r, read_sharpener, w_shift_w, write_sharpener):
+def shift(w_g, shift_weighting):
+    # We'll use shift weightings of length 3 corresponding to [-1, 0 , 1]
+    # Shift weightings are obtained by a softmax attached to the controler
 
 
-def focus(memory_matrix, outputs, prev_w_r, prev_w_w):
-    read_key_vector, write_key_vector,
-    read_key_strength, write_key_strength,
-    read_interpolation_gate, write_interpolation_gate,
-    read_shift_weighting, write_shift_weighting,
-    read_sharpener, write_sharpener, _ = outputs
+def sharpen(w_sft, sharpener):
 
-    w_c_r = content_focus(memory_matrix, read_key_vector, read_key_strength)
-    w_c_w = content_focus(memory_matrix, write_key_vector, write_key_strength)
 
-    w_g_r, w_g_w = interpolate(read_interpolation_gate, w_c_r, prev_w_r,
-        write_interpolation_gate, w_c_w, prev_w_w)
+def focus(memory_matrix, prev_w, key_vector, key_strength, interpolation_gate,
+    shift_weighting, sharpener):
+    w_c = content_focus(memory_matrix, key_vector, key_strength)
+    w_g = interpolate(interpolation_gate, w_c, prev_w)
+    w_sft = shift(w_g, shift_weighting)
+    return sharpen(w_sft, sharpener)
